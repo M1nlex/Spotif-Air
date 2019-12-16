f2f50c7e84cf02d00eb37ab3f280cdceea100071
M1nlex <47282781+M1nlex@users.noreply.github.com>
2019-12-16 01:24:06 +0100

    playlist 2

--- playlist_def.py
+++ playlist_def.py
@@ -1,12 +1,13 @@
 if __name__ == '__main__':
+    liste_essai = [ ["Jigsaw Falling Into Place", "Radiohead"], ["Classic Pursuit", "cYsmix"] ]
     from tkinter import *
     from tkinter import ttk
     from tkinter import font
 
-def testcommand(self, Programme, Name, Number, List, Genre):
-
-    # print(self.Name.get())
-    Playlist_Content(fenetre1)
+def show_playlist(self, Programme, Name, Number, List, Genre):
+    if (Name == "Erreur" and Number == 0):
+        return
+    Playlist_Content(Programme=fenetre1, Name=Name, Number=Number, List=List, Genre=Genre)
     fenetre1.tkraise()
 
 def return_to_playlist(self):
@@ -32,7 +33,7 @@ class Playlist(Frame):
         self.LabelName = Label(self, textvariable = self.Name)
         self.LabelName.pack(side=LEFT, fill=BOTH, expand=1)
 
-        self.Confirm_PL = Button(self, text = "Voir la Playlist", command=lambda:testcommand(self, self.Programme, self.Name, self.Number, self.List, self.Genre))
+        self.Confirm_PL = Button(self, text = "Voir la Playlist", command=lambda:show_playlist(self, self.Programme, self.Name.get(), self.Number.get(), self.List, self.Genre.get()))
         self.Confirm_PL.pack(side=RIGHT, fill=BOTH)
 
         self.LabelGenre = Label(self, textvariable = self.Genre)
@@ -40,6 +41,25 @@ class Playlist(Frame):
 
         self.LabelNumber = Label(self, textvariable = self.Number)
         self.LabelNumber.pack(side=RIGHT, fill=BOTH, expand=1)
+
+class MusicInfo(Frame):
+
+    def __init__(self, Programme=None, Name="", Artist=""):
+
+        self.Name=Name
+        self.Artist=Artist
+
+        Frame.__init__(self, Programme, bd=2, relief="groove")
+        self.pack(fill = BOTH)
+
+        self.labelname = Label(self, text=self.Name)
+        self.labelname.pack(side=LEFT, fill=BOTH, expand=1)
+
+        self.Playbutton = Button(self, text="Jouer")
+        self.Playbutton.pack(side=RIGHT, fill=BOTH)
+
+        self.labelartist = Label(self, text=self.Artist)
+        self.labelartist.pack(side=RIGHT, fill=BOTH, expand=1)
 
 
 
@@ -52,6 +72,8 @@ class Playlist_Content(Frame):
         self.List = List
         self.Genre = StringVar()
 
+
+
         self.Name.set(Name)
         self.Number.set(Number)
         self.Genre.set(Genre)
@@ -60,7 +82,28 @@ class Playlist_Content(Frame):
         self.pack(fill = BOTH)
 
         self.leavebutton = Button(self, text="Playlists", command=lambda:return_to_playlist(self))
-        self.leavebutton.pack()
+        self.leavebutton.grid(row=0, column=0, rowspan=2, sticky="ns")
+
+        self.labelname = Label(self, textvariable=self.Name,)
+        self.labelname.grid(row=0, column=1, rowspan=2, sticky="nsew")
+
+        self.Labelfornumber = Label(self, text="Nombre de musiques :")
+        self.Labelfornumber.grid(row=0, column=3)
+
+        self.LabelNumber = Label(self, textvariable=self.Number)
+        self.LabelNumber.grid(row=0, column=4)
+
+        self.Labelforgenre = Label(self, text="Genre :")
+        self.Labelforgenre.grid(row=1, column=3)
+
+        self.LabelGenre = Label(self, textvariable=self.Genre)
+        self.LabelGenre.grid(row=1, column=4)
+
+        self.Musics = Frame(self, relief=SUNKEN, bd=5, padx=5, pady=5)
+        self.Musics.grid(row=2, column=0, columnspan=4, sticky="nsew")
+
+        for i in self.List:
+            MusicInfo(self.Musics, Name=i[0], Artist=i[1])
 
 
 if __name__ == '__main__':
@@ -73,6 +116,6 @@ if __name__ == '__main__':
     fenetre2.grid(row=0, column=0, sticky = "nsew")
 
     Playlist(Programme = fenetre2, Name="yes", Number=2, Genre="Rock")
-    Playlist(fenetre2, "essai", 4, [], "Classique")
+    Playlist(fenetre2, "essai", 4, liste_essai, "Classique")
     Playlist(fenetre2)
     main.mainloop()
